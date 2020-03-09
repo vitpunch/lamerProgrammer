c5bd4753b5e73278c9f0d0b0f2ef3b2b76b959b4
Vitaly Kulakov <admin@spidometrus.ru>
2020-03-09 18:39:35 +0800

    Сделано отображение рандомной истории

---
+++ lamerProgrammer/Controllers/storyController.cs
@@ -0,0 +1,41 @@
+﻿using lamerProgrammer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lamerProgrammer.Controllers
+{
+    public class storyController : Controller
+    {
+        // GET: story
+        public ActionResult Index()
+        {
+            return View("random");
+        }
+        public ActionResult Random()
+        {
+            Story story = new Story();
+            story.Random();
+            return View(story);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Hot()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Number()
+        {
+            Story story = new Story();
+            return View(story);
+        }
+    }
+}
